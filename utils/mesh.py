54a15b38a08f28ca368a0570719cfa66558dc955
huayizeng <zenghy1993@gmail.com>
2018-10-23 09:14:33 -0500

    Adding mesh reader and projection code

---
+++ utils/mesh.py
@@ -0,0 +1,37 @@
+import re
+import os
+import numpy as np
+
+def read_obj(filename):
+    f = open(filename, "r") 
+    if not f:
+        print ("Cannot open a file: " + filename)
+        return None
+    vertices = []
+    faces = []
+
+    v_pattern = re.compile(r"v\s+([+-]?\d+[.]*\d*)\s+([+-]?\d+[.]*\d*)\s+([+-]?\d+[.]*\d*)\s+([+-]?\d+[.]*\d*)")
+    f_pattern = re.compile(r"f\s+(\d+[.]*\d*)\s+(\d+[.]*\d*)\s+(\d+[.]*\d*)")
+    for line in f:
+        if line[0] == "v":
+            m = line.split()
+            vertices.append( [ float(m[1]), float(m[2]), float(m[3]) ] )
+
+            # m = v_pattern.search(line)
+            # if m:
+            #     vertices.append( [ float(m.group(1)), float(m.group(2)), float(m.group(3)) ] )
+
+        if line[0] == "f":
+            m = f_pattern.search(line)
+            if m:
+                faces.append([int(m.group(1))-1, int(m.group(2))-1, int(m.group(3))-1])
+    f.close()
+    return (vertices, faces)
+
+def write_obj(filename, vertices, faces):
+    with open(filename, "w+") as fout:          
+        for v in vertices:
+            fout.write("v " + str(v[0]) + " " + str(v[1]) + " " + str(v[2]) + "\n")
+        for f in faces:
+            fout.write("f " + str(f[0] + 1) + " " + str(f[1] + 1) + " " + str(f[2] + 1) + "\n")
+    return True
---
+++ utils/projection.py
@@ -0,0 +1,48 @@
+"""
+This file aims at test if the camera model is correct. 
+We render the silhouette as a binary image, and compare it to 'test.png'
+"""
+import numpy as np
+import math
+import os
+import cv2
+
+from mesh import read_obj
+
+def get_cam_matrix():
+    K = np.asarray([[1050.0, 0.0, 480.0],
+        [0.0, 1050.0, 270.0],
+        [0.0, 0.0, 1.0]])
+    RT = np.asarray([[0.6859206557273865, 0.7276763319969177, -4.011331711240018e-09, -0.3960070312023163],
+        [0.32401347160339355, -0.3054208755493164, -0.8953956365585327, 0.3731381893157959],
+        [-0.6515582203865051, 0.6141703724861145, -0.44527140259742737, 11.250574111938477]])
+    return K, RT
+
+if __name__ == '__main__':
+    K, RT = get_cam_matrix()
+    R = RT[:, :3]
+    T = RT[:, 3]
+    img_test = cv2.imread("../data/test.png")
+    print("img_test.shape: ", img_test.shape[:2])
+    k_intensity_background = 50
+    im_reprojection = np.ones(img_test.shape[:2]) * k_intensity_background
+
+    raise NotImplementedError
+    # TODO: Read .obj
+    # TODO: In the following code, pts is sampled points from .obj; However, it's not correct. 
+    # TODO: Instead, we should read .obj files, render each line.     
+    path_pts = "../data/3dv2016.pts"
+    point_set = np.loadtxt(path_pts).astype(np.float32)
+
+    # homog_point = np.append(point_set, np.ones([point_set.shape[0], 1]), 1)
+    # projected_point = np.dot(cam, homog_point.transpose()).transpose()
+    for p in point_set:
+        p = np.dot(R, p) + T
+        p = np.dot(K, p)
+        p0_x = int(p[0] / p[2])
+        p0_y = int(p[1] / p[2])
+        if p0_x < 0 or p0_x >= im_reprojection.shape[1] or p0_y <0 or p0_y >= im_reprojection.shape[0]:
+            continue
+        im_reprojection[p0_y, p0_x] = 255
+    path_image = "im_reprojection.jpg"
+    cv2.imwrite(path_image, im_reprojection)
